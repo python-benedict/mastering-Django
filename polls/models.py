7e7b3fb930d15f3deca2958cb054469ab59dac76
Benedict <newlyf38@gmail.com>
2023-02-12 08:03:08 +0000

    made changes to the models

--- polls/models.py
+++ polls/models.py
@@ -7,10 +7,10 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     
     def __str__(self):
-        return self.pub_date
+        return self.question
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(day=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
 
 class Choice(models.Model):
