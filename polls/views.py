eb4a5bc36ac17ecfb03d3b638d7b65d10b114f67
Benedict <newlyf38@gmail.com>
2023-02-15 15:24:54 +0000

    added another view called detail

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Hello, world. You are at the pool index')
+
+def detail(request, question_id):
+    return HttpResponse("you are looking at question %s" %question_id)
