f6df9c67ef68a6b5c88d31b20834e8ff580d9b54
Benedict <newlyf38@gmail.com>
2023-02-15 15:50:16 +0000

    added another url for the vote view

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name="detail"),
     path('<int:question_id>/result/', views.results, name="result"),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
