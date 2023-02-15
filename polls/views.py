6c61874b2e8704d4dc9a985f86fd89046eed21aa
Benedict <newlyf38@gmail.com>
2023-02-15 16:30:47 +0000

    deleted all the migrations and database

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, world. You are at the pool index')
+    questions = Question.objects.all()
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(questions)
 
 def detail(request, question_id):
     return HttpResponse("you are looking at question %s" %question_id)
