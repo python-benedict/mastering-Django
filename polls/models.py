9a126b6c4ddff0d598f7292abea0245adacd36fc
Benedict <newlyf38@gmail.com>
2023-02-15 16:36:00 +0000

    changed the question field name question_text to question again

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 import datetime
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     
     def __str__(self):
